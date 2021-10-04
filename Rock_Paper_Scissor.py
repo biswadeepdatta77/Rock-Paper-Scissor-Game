5277bcba0664190c4af697bd01c5a0e7709cd3d8
biswadeepdatta77 <91516122+biswadeepdatta77@users.noreply.github.com>
2021-10-04 12:36:23 +0530

    Update Rock_Paper_Scissor.py

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -28,7 +28,19 @@ game2= ['rock', 'scissor', 'paper']
 computer_hand = random.choice(game2)
 print("Welcome to Biswadeep Datta's brand new game Rock Paper Scissor.Rules: Type rock or scissor or paper")
 user_hand = input("Enter your turn among rock paper scissor ")
+print("You chose:")
+if(user_hand=='rock'):
+    print(rock)
+if(user_hand=='paper'):
+    print(paper)
+
+if(user_hand=='scissor'):
+    print(scissor)
+
+print("Computer chose: ")
+
 if computer_hand=='rock':
+
     print(rock)
     if computer_hand==user_hand:
         print("Draw")
